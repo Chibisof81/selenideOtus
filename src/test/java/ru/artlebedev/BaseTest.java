fc99464617d82ffc340c5c09e2aece858e131f69
Chibisof81 <chibisof81@yandex.ru>
2018-06-11 16:40:00 +0300

    добавил логи

--- src/test/java/ru/artlebedev/BaseTest.java
+++ src/test/java/ru/artlebedev/BaseTest.java
@@ -3,11 +3,10 @@ package ru.artlebedev;
 import com.codeborne.selenide.Configuration;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.testng.annotations.BeforeTest;
-import ru.artlebedev.pages.MainPage;
 
 public class BaseTest {
     @BeforeTest
-    public void beforeTest(){
+    public void beforeTest() {
         Configuration.browser = "chrome";
         WebDriverManager.chromedriver().setup();
     }
